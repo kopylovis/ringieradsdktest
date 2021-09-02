0974e5c0a04866e3e1c59210a9231084a73958d8
iAgentur <info@iagentur.ch>
2021-09-02 18:13:58 +0300

    release v.0.1.13

---
+++ DokkaDocs/dokka/html/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":app:ringieradsdk:dokkaHtml/androidTestRelease":[],":app:ringieradsdk:dokkaHtml/debug":[],":app:ringieradsdk:dokkaHtml/main":[],":app:ringieradsdk:dokkaHtml/release":[]}'
